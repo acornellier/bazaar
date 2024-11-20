a6a64d88afd225ba0c238d12a46174a98ff45571
Alex <alex@jintronix.com>
2024-11-20 09:43:07 -0500

    sort items by hero

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,5 +1,13 @@
 ﻿import itemsJson from './items.json'
 import type { Item } from './types.ts'
+import { heroes } from './heroes.ts'
 
 // @ts-ignore
-export const allItems = itemsJson as Item[]
+export const allItems = (itemsJson as Item[]).sort((a, b) => {
+  const heroA = a.heroes[0]
+  const heroB = b.heroes[0]
+  if (heroA && heroB && heroA !== heroB) {
+    return heroes.indexOf(heroA) - heroes.indexOf(heroB)
+  }
+  return a.name.localeCompare(b.name)
+})
